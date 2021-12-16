b98cd73bbb356cd89f2651e0a0a8834a515692fc
werto87 <huhulasmichinruhe@web.de>
2021-12-16 23:44:22 +0100

    removed not needed code

--- src/logic/logic.cxx
+++ src/logic/logic.cxx
@@ -60,12 +60,6 @@
 #include <variant>
 #include <vector>
 
-template <class... Ts> struct overloaded : Ts...
-{
-  using Ts::operator()...;
-};
-template <class... Ts> overloaded (Ts...) -> overloaded<Ts...>;
-
 void
 removeGameIfItIsOver (std::string const &accountName, std::list<GameMachine> &gameMachines)
 {
